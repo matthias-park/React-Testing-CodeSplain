3ab6712bd24579de3aad5f1b1c802a246c3b9e75
Junsoo <junsoo@strivegaming.com>
2023-12-13 11:47:09 -0800

    Feat: Add a github Link and Async Act Testing

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -2,22 +2,63 @@ import { screen, render } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import RepositoriesListItem from "./RepositoriesListItem";
 
+// jest.mock('../tree/FileIcon.js', () => {
+//   //content of FileIcon.js
+//   return () => {
+//     return 'File Icon Component'
+//   }
+// });
+
 function renderComponent() {
   const repository = {
-    full_name: 'facebook/react',
-    language: 'Javascript',
-    description: 'A js library',
-    owner: 'facebook',
-    name: 'react',
-    html_url: 'https://github.com/facebook/react'
+    full_name: "facebook/react",
+    language: "Javascript",
+    description: "A js library",
+    owner: {
+      login: "facebook",
+    },
+    name: "react",
+    html_url: "https://github.com/facebook/react",
   };
   render(
     <MemoryRouter>
-      <RepositoriesListItem repository={repository}/>
+      <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
 }
 
-test("shows a link to the github homepage for this repository", () => {
+test("shows a link to the github homepage for this repository", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = screen.getByRole("link", {
+    name: /github repository/i,
+  });
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a fileicon with the appropriate icon", async () => {
   renderComponent();
+
+  const icon = await screen.findByRole("img", { name: "Javascript" });
+
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  expect(link).toHaveAttribute(
+    "href",
+    `/repositories/${repository.full_name}`
+  );
 });
