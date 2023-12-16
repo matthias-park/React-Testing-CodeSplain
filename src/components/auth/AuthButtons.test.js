ce0673c9771a84804a683e85f2dc2995d7ad2d0e
Junsoo <junsoo@strivegaming.com>
2023-12-16 10:00:51 -0800

    feat: Scooping Test Hooks

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -1,22 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { createServer } from "../../test/server";
-import AuthButtons from './AuthButtons';
+import AuthButtons from "./AuthButtons";
 
-// createServer() ---> GET '/api/user' ---> {user:null}
-test('when user is not sign in, sign in the sign up are visible', async () => {
+describe("when user is not signed in", () => {
+  // createServer() ---> GET '/api/user' ---> {user:null}
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: null };
+      },
+    },
+  ]);
+  test("when user is not sign in, sign in the sign up are visible", async () => {});
 
+  test("test user is not signed in, sign out is not visible", async () => {});
 });
 
-test('test user is not signed in, sign out is not visible', async () => {
+describe("when user is signed in", () => {
+  // createServer() ---> GET '/api/user' ---> { user: {id: 3, email: 'asdf@a.com'}}
+  createServer([
+    {
+      path: "/api/user",
+      res: () => {
+        return { user: { id: 3, email: "asdf@a.com" } };
+      },
+    },
+  ]);
+  test("when user is signed in, sign in and sign up are not visible", async () => {});
 
+  test("when user is signed in, sign out is visible", async () => {});
 });
-
-// createServer() ---> GET '/api/user' ---> { user: {id: 3, email: 'asdf@a.com}}
-test('when user is signed in, sign in and sign up are not visible', async () => {
-
-})
-
-test('when user is signed in, sign out is visible', async () => {
-
-})
